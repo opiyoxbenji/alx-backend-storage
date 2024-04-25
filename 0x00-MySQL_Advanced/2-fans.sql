f08deb95b7cc16793fa045b7e8112acca7200431
opiyoxbenji <opiyosbenji@gmail.com>
2024-04-25 13:28:43 +0000

    ranks country origins of bands

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,9 +1,3 @@
--- 
-SELECT origin, COUNT(*) AS nb_fans
-FROM (
-	SELECT DISTINCT m.name, m.origin, f.user_id
-	FROM metal_bands m
-	JOIN fans f ON  m.name = f.band_name
-) AS subquery
-GROUP BY origin
-ORDER BY nb_fans DESC;
+-- ranks country origins of bands
+SELECT origin, SUM(fans) as nb_fans from metal_bands
+GROUP BY origin ORDER BY nb_fans DESC;
