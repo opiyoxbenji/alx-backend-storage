7bc882b69105d30fbc236f997bc3ae09c1d44ffc
opiyoxbenji <opiyosbenji@gmail.com>
2024-04-25 09:51:11 +0000

    alternative

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -1,6 +1,11 @@
 -- Creates a view need_meeting
 CREATE VIEW need_meeting AS
 SELECT name
-FROM students
+FROM students s
 WHERE score < 80
-AND (last_meeting is NULL OR last_meeting < DATE_SUB(NOW(), INTERVAL 1 MONTH));
+AND NOT EXISTS (
+	SELECT 1
+	FROM meetings M
+	WHERE m.student_id = s.id
+	AND m.meeting_data > DATE_SUB(NOW(), INTERVAL 1 MONTH)
+);
