1cc9dde001d28f530c6e2a8e43e38ef6fa5a3537
opiyoxbenji <opiyosbenji@gmail.com>
2024-04-25 09:47:26 +0000

    creates a view named need_meeting that selects the names of students who meet the specified conditions

---
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,6 @@
+-- Creates a view need_meeting
+CREATE VIEW need_meeting AS
+SELECT name
+FROM students
+WHERE score < 80
+AND (last_meeting is NULL OR last_meeting < DATE_SUB(NOW(), INTERVAL 1 MONTH));
