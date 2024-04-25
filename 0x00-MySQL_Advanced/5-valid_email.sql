463b7b5b469f05b98867776e4472d35f17d1b7c7
opiyoxbenji <opiyosbenji@gmail.com>
2024-04-25 17:12:39 +0000

    tasks

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,10 @@
+-- Creates a trigger that resets the attribute
+DELIMITER $$ ;
+CREATE TRIGGER validate BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+	IF NEW.email != OLD.email THEN
+		SET NEW.valid_email = 0;
+	END IF;
+END;$$
+delimiter ;
