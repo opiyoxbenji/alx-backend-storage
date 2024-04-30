7a1ab014e1008daefe9696399bd86fff138e9dfc
opiyoxbenji <opiyosbenji@gmail.com>
2024-04-30 12:04:19 +0000

    returns the lists of schools having a specific topic

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Task 11 - modules
+"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    Returns the list of schools having a specific topic
+    """
+    topic_filter = {
+            'topic': {
+                '$elemMatch': {
+                    '$eq': topic,
+                },
+            },
+    }
+    return [doc for doc in mongo_collection.find(topic_filter)]
